57263ababfeb80e3942ecb20d0ac1f817d0acd23
szintia <kasziba.cintia@gmail.com>
2024-03-20 21:42:19 +0100

    started using micrometer tracing

--- src/main/java/com/example/orderservice/client/OrderDetailsClient.java
+++ src/main/java/com/example/orderservice/client/OrderDetailsClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "order-details-service")
+@FeignClient(url="${url:http://localhost:8080}", name = "order-details-service")
 public interface OrderDetailsClient {
 
     @PostMapping("/order-details")
--- src/main/java/com/example/orderservice/service/ValidationService.java
+++ src/main/java/com/example/orderservice/service/ValidationService.java
@@ -1,27 +1,43 @@
 package com.example.orderservice.service;
 
+import com.example.orderservice.client.OrderDetailsClient;
 import com.example.orderservice.model.Order;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class ValidationService {
+    private static final Logger log = LoggerFactory.getLogger(ValidationService.class);
     private OrderService orderService;
+    private OrderDetailsClient orderDetailsClient;
+    private Tracer tracer;
 
     public ValidationService() {
     }
 
-    public ValidationService(OrderService orderService) {
+    public ValidationService(OrderService orderService, OrderDetailsClient orderDetailsClient, Tracer tracer) {
         this.orderService = orderService;
+        this.orderDetailsClient = orderDetailsClient;
+        this.tracer = tracer;
     }
 
     public Mono<Order> validate(Order order) {
-        if (order.getCustomer() == null) {
-            return null;
+        Span span = this.tracer.nextSpan();
+
+        try (Tracer.SpanInScope ws = this.tracer.withSpan(span.start())) {
+            log.info("<TRACE:{}> ", this.tracer.currentSpan().context().traceId());
+
+            if (order.getCustomer() == null) {
+                return null;
+            }
+            Order savedOrder = orderService.persistOrder(order);
+            return Mono.just(savedOrder);
+        } finally {
+            span.end();
         }
-
-        Order savedOrder = orderService.persistOrder(order);
-
-        return Mono.just(savedOrder);
     }
 }
